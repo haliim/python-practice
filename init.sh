0d90bc302f435254a4a6602ba8c081a39a8e9931
Ahmed Halim <ah.hesham93@gmail.com>
2022-01-16 19:38:16 +0200

    playing with django

---
+++ init.sh
@@ -0,0 +1,11 @@
+/usr/bin/python3 -m pip install --upgrade pip   #upgrade pip
+pip3 install pipenv #Install pipenv for managing virtual enviroments
+mkdir django
+cd django
+pipenv install django
+pipenv --venv   # --> will show the path to the venv like the below line
+cd /home/halim/.local/share/virtualenvs/<env dir name>  # /bin/python
+cd ~/python-practice/django
+pipenv shell    #To enter the virtual env under the django dir
+django-admin startproject <proj name> .
+python manage.py runserver <port num>   #port by default is 8000
