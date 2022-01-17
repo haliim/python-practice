46225214eff403af2e63f2efa3e69dbf9efe2ce8
Ahmed Halim <ah.hesham93@gmail.com>
2022-01-18 01:04:39 +0200

    solving runner challenge

--- 2- DataTypes/runner.py
+++ 2- DataTypes/runner.py
@@ -1,3 +1,6 @@
 if __name__ == '__main__':
     n = int(input())
-    arr = map(int, input().split())
+    arr = list(map(int, input().split()))
+    sorted_unique_list = list(set(arr))
+    sorted_list = sorted(sorted_unique_list, reverse=True)
+    print(sorted_list[1])
