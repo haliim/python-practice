344e31554ecb36b8a2fda60404971f0e1cb5a6d9
Ahmed Halim <ah.hesham93@gmail.com>
2022-10-14 00:14:49 +0200

    mutations

--- Hackerrank/mutations.py
+++ Hackerrank/mutations.py
@@ -1,12 +1,12 @@
 def mutate_string(string, position, character):
-    list_string = list(string)
-    list_string(position) = character
-    string = ''.join(list_string)
-    print(string)
-    return
+    #list_string = list(string)
+    #list_string(position) = character
+    #string = ''.join(list_string)
+    
+    return string[:position] + character + string[position + 1:]
 
 if __name__ == '__main__':
-    s = input("please write custome string")
-    i, c = input("choose character number").split()
+    s = input("please write custome string: ")
+    i, c = input("choose character number, then space and string value: ").split()
     s_new = mutate_string(s, int(i), c)
     print(s_new)
